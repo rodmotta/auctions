661984025f28baa63ddbb425e9a8928f4199c01b
Rodrigo <rodmotta777@gmail.com>
2025-05-19 20:15:24 -0300

    project setup

---
+++ bid-service/bid-service/src/main/java/com/github/rodmotta/bid_service/BidServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.rodmotta.bid_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidServiceApplication.class, args);
+	}
+
+}
